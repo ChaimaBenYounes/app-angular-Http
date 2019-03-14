6d66eb69b470af8449a2d5a26b2a4f27f071d22e
chaimaby <chaima.benyounes1@gmail.com>
2019-03-14 16:20:04 +0100

    [add] new componenet youtube search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SimpleHttpComponent } from './simple-http/simple-http.component';
-import { YouTubeSearchService } from './services/youtube-search.service'
+import { YouTubeSearchService } from './services/youtube-search.service';
+import { YoutubeSearchComponent } from './youtube-search/youtube-search.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleHttpComponent
+    SimpleHttpComponent,
+    YoutubeSearchComponent
   ],
   imports: [
     BrowserModule,
