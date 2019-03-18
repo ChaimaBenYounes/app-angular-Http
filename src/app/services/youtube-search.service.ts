3b05dbb81c18905b435e7403cd86f11a306e4121
chaimaby <chaima.benyounes1@gmail.com>
2019-03-18 14:22:36 +0100

    [Update] YOUTUBE_API_KEY change browser key with  server key

--- src/app/services/youtube-search.service.ts
+++ src/app/services/youtube-search.service.ts
@@ -15,7 +15,7 @@
     your browser is "localhost"
   */
   export const YOUTUBE_API_KEY =
-    'AIzaSyCqB8_rFxtK_1_pih5z79Yo16htAW9KoI4';
+    'AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk';
   export const YOUTUBE_API_URL =
     'https://www.googleapis.com/youtube/v3/search';
   
